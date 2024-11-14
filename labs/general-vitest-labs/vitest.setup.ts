38297b428f4570ed7cf3baf58d022753ca28cd8f
Joostlambregts <joost.lambregts@amis.nl>
2024-11-14 14:41:50 +0100

    powerpoint update

--- labs/general-vitest-labs/vitest.setup.ts
+++ labs/general-vitest-labs/vitest.setup.ts
@@ -7,3 +7,7 @@ beforeEach (() => {
 afterEach(() => {
   vi.useRealTimers()
 })
+
+vi.setSystemTime(new Date(2021, 1, 1))
+vi.advanceTimersByTime(1000) // Sometimes fails with asynchronous code
+vi.advanceTimersByTimeAsync(1000) // Always works
